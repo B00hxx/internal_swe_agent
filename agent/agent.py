35b800520a1f8ba0c3d59aa912404dca32833733
B00hxx <cataloniachangedme@gmail.com>
2026-09-26 11:05:05 +0100

    feat: started working on the agent class

---
+++ agent/agent.py
@@ -0,0 +1,45 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END, MessagesState
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.messages import SystemMessage
+
+class Agent:
+    def __init__(self, tools : list, alias : str, system_prompt : str = "You are a helpful AI assistant. /no_think"):
+        self.tools = tools
+        self.alias = alias
+        self.system = SystemMessage(system_prompt)
+        self.client = self._get_client().bind_tools(self.tools)
+    
+    def _get_client(self) -> ChatOpenAI:
+        return ChatOpenAI(
+            base_url=os.environ["LITELLM_BASE_URL"],
+            api_key=os.environ["LITELLM_API_KEY"],
+            model=self.alias,
+            temperature=0.7,
+        )
+
+    def _call_model(self, state : MessagesState):
+        response = self.client.invoke([self.system] + state["messages"])
+        return {"messages": [response]}
+
+    
+    def build_graph(self):
+        builder = StateGraph(MessagesState)
+        builder.add_node("agent", self._call_model)
+        builder.add_node("tools", ToolNode(self.tools))
+        builder.add_edge(START, "agent")
+        builder.add_conditional_edges("agent", tools_condition)
+        builder.add_edge('tools', "agent")
+        return builder.compile()
+
+    def run(self, prompt: str, recursion_limit: int = 10):
+        inputs = {"messages": [("user", prompt)]}
+        config = {"recursion_limit": recursion_limit}
+        for chunk in self.graph.stream(inputs, config, stream_mode="updates"):
+            for node, update in chunk.items():
+                print(f"--- {node} ---")
+                update["messages"][-1].pretty_print()
---
+++ tools/represent_repo.py
@@ -0,0 +1,11 @@
+from langchain_core.tools import tool
+from pathlib import Path
+from repo_representation import RepoMap
+
+
+@tool
+def represent_repo(path : Path) -> dict:
+    """This tool can be used to return a dictionary which contains the representation of the repo through a provided path"""
+    root = RepoMap(root = path)
+    return root.serialize()
+
